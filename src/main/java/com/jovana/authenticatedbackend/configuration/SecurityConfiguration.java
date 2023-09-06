5b0f05a0f74b83a148919ff33dc4ce6a744b12f5
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-09-06 12:50:43 +0200

    Added passwordEncoder and authManager

--- src/main/java/com/jovana/authenticatedbackend/configuration/SecurityConfiguration.java
+++ src/main/java/com/jovana/authenticatedbackend/configuration/SecurityConfiguration.java
@@ -2,11 +2,30 @@ package com.jovana.authenticatedbackend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authManager(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
+        daoProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(daoProvider);
+    }
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
